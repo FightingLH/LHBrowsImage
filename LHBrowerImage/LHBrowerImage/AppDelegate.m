3b647cb0256081af2de0f2af612c5a8129d271a9
lihuan <976244150@qq.com>
2016-10-12 15:58:48 +0800

    hello everyone

--- LHBrowerImage/LHBrowerImage/AppDelegate.m
+++ LHBrowerImage/LHBrowerImage/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
      self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:[ViewController new]];
-    
+    NSLog(@"test");
     return YES;
 }
 
