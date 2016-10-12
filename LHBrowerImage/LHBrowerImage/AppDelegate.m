d6ab0baa1e5f931c644c0bd54449e4fe3ba3d1af
lihuan <976244150@qq.com>
2016-10-12 15:55:45 +0800

    hello# Please enter the commit message for your changes. Lines starting

--- LHBrowerImage/LHBrowerImage/AppDelegate.m
+++ LHBrowerImage/LHBrowerImage/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
      self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:[ViewController new]];
+    
     return YES;
 }
 
